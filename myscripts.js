37c79fdf3ad04293295e25d1fbeb591877d15bbb
Ravi Choudhary <ravichoudhary09@yahoo.com>
2022-11-13 04:28:15 +0530

    form work started

--- myscripts.js
+++ myscripts.js
@@ -21,4 +21,31 @@ $(document).ready(function(){
             }
         }
     })
-  });
+
+    $(".submitForm").on("click", function(){
+      let formData ={};
+      var full_name = $('#full_name').val();
+      if(full_name){
+        formData.full_name =full_name;
+      }
+      var email = $('#email').val();
+      if(email){
+        formData.email =email;
+      }
+      var contact_no = $('#contact_no').val();
+      if(contact_no){
+        formData.contact_no =contact_no;
+      }
+      var subject = $('#subject').val();
+      if(subject){
+        formData.subject =subject;
+      }
+      var message = $('#message').val();
+      if(message){
+        formData.message =message;
+      }
+   
+      });
+
+  });//Ready function end
+
